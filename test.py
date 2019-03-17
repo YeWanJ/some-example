6b8fbe990c4bada1d4d32e3aa1bbc4ad1a02d91a
YeWanJ <yej97886652@gmail.com>
2019-03-17 12:35:45 +0800

    add problem3.py

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-def calcE():
-    x = y = 1
-    sum = 0
-    while (sum < 1000000):
-        sum += (x + y)
-        x, y = x + 2 * y, 2 * x + 3 * y
-    return sum
+i = 1
+for k in (range(1, 21)):
+    if i % k > 0:
+        for j in range(1, 21):
+            if (i*j) % k == 0:
+                i *= j
+                break
+print(i)
+
+num = 600851475143
+for m in range(2, num):
+    if m * m <= num:
+        if num % m == 0:
+            large = num / m
+            if Pri(large):
+                print("The answer is: ", large)
