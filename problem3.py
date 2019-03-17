cb9ef4556eb4439e80cd48f961df885f5ac147ed
YeWanJ <yej97886652@gmail.com>
2019-03-17 12:34:13 +0800

    add problem.py

--- problem3.py
+++ problem3.py
@@ -1,2 +1,29 @@
-def prime(self, parameter_list):
-    while
+from sys import exit
+
+
+def Pri(N):
+    for i in range(2, N):
+        for j in range(2, N):
+            if i * j == N:    # 证明非质数
+                return False
+        if i * i <= N:
+            continue
+        else:
+            return False
+    return True
+
+
+num = 600851475143
+for m in range(2, num):
+    if m * m <= num:
+        if num % m == 0:
+            large = num // m
+            if Pri(large):
+                print("The answer is: ", large)
+                exit(0)
+            else:
+                continue
+        else:
+            continue
+    else:
+        continue
